e62296fa0386a804a434b473de3098c91a1f988a
JGrigolo <jgrigolo@gmail.com>
2021-12-01 11:14:23 -0300

    Update numero_por_extenso.py

--- numero_por_extenso.py
+++ numero_por_extenso.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License     #
 # along with this program.  If not, see <https://www.gnu.org/licenses/> #
 #                                                                       #
-#                       Joel Grigolo - https://matehacker.org - 06/2020 #
+#                       Joel Grigolo - https://matehacker.org - 12/2021 #
 #########################################################################
 
 def por_extenso(n):
